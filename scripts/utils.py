081e7b177a61d0cbb991ae8410398ab526e1ed82
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-07-15 17:30:05 +0800

    prepare for actually really true final training run.docx

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,10 @@
+from TCN.TCN.tcn import TCN_DimensionalityReduced
 import sys
 import os
 
 import json
 import signal
+from typing import Callable
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,8 +17,10 @@ from torch import nn
 from torch.utils.data import Dataset, DataLoader, Subset
 
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, auc, precision_recall_curve
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, auc, precision_recall_curve, roc_auc_score
 from sklearn.metrics import roc_curve as sk_roc_curve
+from sklearn.preprocessing import LabelBinarizer
+from scipy.interpolate import interp1d
 
 from fastai.learner import Learner
 from fastai.metrics import accuracy, Precision, Recall, F1Score, RocAuc
@@ -25,9 +29,17 @@ from fastai.callback.fp16 import MixedPrecision
 from fastai.metrics import skm_to_fastai
 
 sys.path.append("../" + os.path.dirname(os.path.realpath(__file__)))
-from TCN.TCN.tcn import TCN_DimensionalityReduced
+
 
 class ECGDataset(Dataset):
+    """ECG Dataset class.
+
+    Args:
+        data_dir (str): path to data directory
+        item_transform (Callable, optional): item transform. Defaults to None.
+        target_transform (Callable, optional): target transform. Defaults to None.
+        in_memory (bool, optional): whether to load all data into memory. Defaults to True.
+    """
     def __init__(self, data_dir, item_transform=None, target_transform=None, in_memory=True) -> None:
         self.data_dir = data_dir
         self.lengths = {}
@@ -47,11 +59,28 @@ class ECGDataset(Dataset):
             self.X = np.array([hb for (hb, _) in self.data])
             self.y = np.array([label for (_, label) in self.data])
             del self.data
+
     def __len__(self) -> int:
+        """Returns length of dataset.
+
+        Returns:
+            int: dataset length
+        """
         return sum(self.lengths.values())
-    
+
     def __getitem__(self, idx):
-        assert idx < self.__len__() # make sure we're not out of bounds
+        """Gets item at index idx.
+
+        Args:
+            idx (int): index
+
+        Raises:
+            IndexError: if index is out of bounds
+
+        Returns:
+            (np.ndarray, str): heartbeat signal and label
+        """
+        assert idx < self.__len__()  # make sure we're not out of bounds
         if not self.in_memory:
             running_count = 0
             for file, length in self.lengths.items():
@@ -70,16 +99,23 @@ class ECGDataset(Dataset):
             hb = self.X[idx]
             label = self.y[idx]
             hb = self.item_transform(hb) if self.item_transform else hb
-            label = self.target_transform(label) if self.target_transform else label
+            label = self.target_transform(
+                label) if self.target_transform else label
             return hb, label
         # should never get here
         raise IndexError("Index out of bounds")
 
+    def get_labels(self):
+        return self.y
+
+
 def one_hot_encode(label, classes=["N", "S", "V", "F", "Q"]):
     return torch.eye(len(classes))[classes.index(label)]
 
+
 def one_hot_decode(label, classes=["N", "S", "V", "F", "Q"]):
     return classes[label.argmax()]
+
 
 def noise_at_frequencies(
     hb,
@@ -93,8 +129,10 @@ def noise_at_frequencies(
         freq = np.random.uniform(*freq_range)
         amp = np.random.uniform(*amp_range)
         phase = np.random.uniform(0, 2 * np.pi)
-        noise += amp * np.sin(2 * np.pi * freq * np.arange(hb.shape[0]) / fs + phase)
+        noise += amp * np.sin(2 * np.pi * freq *
+                              np.arange(hb.shape[0]) / fs + phase)
     return hb + noise
+
 
 def noise_at_frequencies_tensor(
     hb: torch.Tensor,
@@ -103,27 +141,77 @@ def noise_at_frequencies_tensor(
     },
     fs=360
 ):
+    """Generates noise at specified frequencies and adds it to a heartbeat signal.
+
+    Args:
+        hb (torch.Tensor): heartbeat signal
+        frequencies_distribution (dict, optional): distributions of noise frequencies and amplitudes. Defaults to { "breathing": ([1/18, 1/12], [1/5, 1/3]) }.
+        fs (int, optional): sampling rate. Defaults to 360.
+
+    Returns:
+        torch.tensor: augmented heartbeat signal
+    """
     noise = torch.zeros(hb.shape)
     for (source, (freq_range, amp_range)) in frequencies_distribution.items():
         freq = torch.distributions.uniform.Uniform(*freq_range).sample()
         amp = torch.distributions.uniform.Uniform(*amp_range).sample()
         phase = torch.distributions.uniform.Uniform(0, 2 * np.pi).sample()
-        noise += amp * torch.sin(2 * np.pi * freq * torch.arange(hb.shape[0]) / fs + phase)
+        noise += amp * torch.sin(2 * np.pi * freq *
+                                 torch.arange(hb.shape[0]) / fs + phase)
     return hb + noise
 
+
 def z_normalise(hb):
+    """Normalises a heartbeat signal to zero mean and unit variance.
+
+    Args:
+        hb (np.ndarray): heartbeat signal
+
+    Returns:
+        np.ndarray: normalised heartbeat signal
+    """
     return (hb - hb.mean()) / hb.std()
 
+
 def hb_transform(hb):
+    """Transforms a heartbeat signal.
+
+    Args:
+        hb (np.ndarray): heartbeat signal
+
+    Returns:
+        torch.tensor: transformed heartbeat signal
+    """
     hb = torch.from_numpy(hb)
     hb = noise_at_frequencies_tensor(hb)
     hb = z_normalise(hb)
     return hb
 
+
 def label_encode(label, classes=["N", "S", "V", "F", "Q"]):
+    """Encodes a label as an integer.
+
+    Args:
+        label (str): label to encode
+        classes (list, optional): class list. Defaults to ["N", "S", "V", "F", "Q"].
+
+    Returns:
+        int: encoded label
+    """
     return classes.index(label)
 
+
 class TCN(nn.Module):
+    """Temporal Convolutional Network (TCN) with dimensionality reduction.
+
+    Args:
+        input_size (int): The length of the input vector.
+        output_size (int): The length of the output vector.
+        num_channels (list): A list of integers, where each integer is the number of channels in a convolutional layer.
+        kernel_size (int): The size of the kernel in each convolutional layer.
+        dropout (float): The dropout rate to use in each convolutional layer.
+        use_skip_connections (bool): Whether to use skip connections between convolutional layers.
+    """
     def __init__(self, input_size, output_size, num_channels, kernel_size, dropout, use_skip_connections=False):
         super(TCN, self).__init__()
         self.tcn = TCN_DimensionalityReduced(
@@ -144,8 +232,13 @@ class TCN(nn.Module):
         o = self.linear(y[:, :, -1])
         return o
 
+
 class LogInterruptable(Callback):
-    def __init__(self, fn:str, **kwargs):
+    """Callback for logging model training progress to a file, for use when resuming training from an interrupted state.
+    Args:
+        fn (str): The filename to save the interrupt information to.
+    """
+    def __init__(self, fn: str, **kwargs):
         super().__init__(**kwargs)
         self.fn = fn
         if os.path.exists(self.fn):
@@ -170,40 +263,27 @@ class LogInterruptable(Callback):
         with open(self.fn, "w") as f:
             json.dump(self.interrupt_info, f, indent=4)
 
-def k_fold_cross_val_predictions(skf:StratifiedKFold, model, model_dir, model_fn, train_set, batch_size=4096, shuffle=True, num_workers=0, pin_memory=True):
-    model_outputs = []
-    classification_reports = []
-    train_ys = np.array([y for _, y in train_set])
-    for fold, (train_idx, val_idx) in enumerate(skf.split(np.zeros(len(train_ys)), train_ys)):
-        print(f"--- Fold {fold + 1} ---")
-        val_fold_set = Subset(train_set, val_idx)
-        val_fold_loader = DataLoader(val_fold_set, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=pin_memory)
-        preds = []
-        y = []
-        learner = Learner(model.cuda(), val_fold_loader, loss_func=nn.CrossEntropyLoss(), cbs=[MixedPrecision()])
-        learner.load(model_dir + f"{fold}" + "/" + model_fn)
-        preds, y = learner.get_preds(dl=val_fold_loader)
-        classification_reports.append(
-            classification_report(
-                y.cpu().numpy(),
-                preds.argmax(dim=1).cpu().numpy(),
-                output_dict=True,
-                labels=["N", "S", "V", "F", "Q"]
-            )
-        )
-        model_outputs.append({
-            "preds": preds,
-            "y": y,
-        })
-    return model_outputs, classification_reports
 
 def k_fold_inference(model, test_dl, model_dir="./prototyping/tcn_fold_", weights_fn="best", k=10):
+    """Performs inference on a k-fold model
+
+    Args:
+        model (nn.Module): the model to perform inference on
+        test_dl (torch.utils.data.DataLoader): dataloader for the test set
+        model_dir (str, optional): directory for all the model weights for each fold. Defaults to "./prototyping/tcn_fold_".
+        weights_fn (str, optional): filename for weights file, .pth file suffix unnecessary. Defaults to "best".
+        k (int, optional): number of folds. Defaults to 10.
+
+    Returns:
+        (list, list): model_outputs, classification_reports for each fold
+        model_outputs: list of dicts with keys "preds", "proba", and "y"
+        classification_reports: list of classification reports for each fold
+    """
     model_outputs = []
     classification_reports = []
     for fold in range(k):
-        # model.load_state_dict(torch.load(model_dir + f"{fold + 1}" + "/" + weights_fn))
-        # model = model.cuda()
-        learner = Learner(model=model.cuda(), dls=test_dl, loss_func=nn.CrossEntropyLoss(), cbs=[MixedPrecision()])
+        learner = Learner(model=model.cuda(), dls=test_dl,
+                          loss_func=nn.CrossEntropyLoss(), cbs=[MixedPrecision()])
         print(model_dir + f"{fold + 1}" + "/" + weights_fn)
         learner.load(model_dir + f"{fold + 1}" + "/" + weights_fn)
         proba, y = learner.get_preds(dl=test_dl)
@@ -223,73 +303,121 @@ def k_fold_inference(model, test_dl, model_dir="./prototyping/tcn_fold_", weight
         })
     return model_outputs, classification_reports
 
-def k_fold_prc_curve(model_outputs, model_name:str):
-    fig, ax = plt.subplots(figsize=(8.27, 11.69), dpi=100)
-    # tprs, aurocs, tpr_threshes = [], [], []
-    # fpr_mean = np.linspace(0, 1, 1000)
+
+def k_fold_roc_curve(model_outputs, model_name: str, num_classes=5, average="macro"):
+    fig, ax = plt.subplots(2, 1, figsize=(8.27, 11.69), dpi=100)
+    tprs, aurocs, tpr_threshes = [], [], []
+    fpr_mean = np.linspace(0, 1, 1000)
     precisions, auprcs, recall_threshes = [], [], []
     recall_mean = np.linspace(0, 1, 1000)
     for fold_idx, fold in enumerate(tqdm(model_outputs)):
-        # ROC Curve
-        # fpr, tpr, tpr_thresh = sk_roc_curve(fold['y'], fold['proba'].detach().cpu().numpy()[:, 1])
-        # tpr_threshes.append(tpr_thresh[np.abs(tpr-0.85).argmin()])
-        # tpr_interp = np.interp(fpr_mean, fpr, tpr)
-        # tpr_interp[0] = 0.0
-        # tprs.append(tpr_interp)
-        # aurocs.append(auc(fpr, tpr))
-        # ax[0].plot(fpr, tpr, label=f"ROC Fold {fold_idx + 1} (AUC = {aurocs[fold_idx]:0.2f})")
+        roc_label_binarizer = LabelBinarizer().fit(fold["y"])
+        y_onehot_test = roc_label_binarizer.transform(fold["y"])
+        intermediate_tprs, intermediate_tpr_threshes = [], []
+        intermediate_precisions, intermediate_recall_threshes, intermediate_auprcs = [], [], []
+        for i in range(num_classes):
+            if i not in fold["y"]:
+                continue
+            # ROC Curve
+            fpr, tpr, tpr_thresh = sk_roc_curve(
+                y_onehot_test[:, i], fold['proba'].detach().cpu().numpy()[:, i])
+            intermediate_tpr_threshes.append(
+                tpr_thresh[np.abs(tpr-0.85).argmin()])
+            tpr_interp = np.interp(fpr_mean, fpr, tpr)
+            tpr_interp[0] = 0.0
+            intermediate_tprs.append(tpr_interp)
+            # PRC
+            precision, recall, prc_thresh = precision_recall_curve(
+                y_onehot_test[:, i], fold['proba'].detach().cpu().numpy()[:, i])
+            prec_interp = np.interp(recall_mean, recall[::-1], precision[::-1])
+            intermediate_precisions.append(prec_interp)
+            intermediate_recall_threshes.append(
+                prc_thresh[np.abs(recall-0.85).argmin()])
+            intermediate_auprcs.append(auc(recall, precision))
+        if average == "macro":
+            tprs.append(np.mean(intermediate_tprs, axis=0))
+            aurocs.append(roc_auc_score(y_onehot_test, fold["proba"].detach().cpu().numpy(
+            ), average="macro", multi_class="ovr", labels=list(set(fold["y"].numpy().tolist()))))
+            tpr_threshes.append(np.mean(intermediate_tpr_threshes))
+            precisions.append(np.mean(intermediate_precisions, axis=0))
+            auprcs.append(auc(recall_mean, precisions[-1]))
+            recall_threshes.append(np.mean(intermediate_recall_threshes))
+        elif average == "weighted":
+            class_distributions = np.bincount(fold["y"])
+            aurocs.append(roc_auc_score(
+                y_onehot_test,
+                fold["proba"].detach().cpu().numpy(),
+                average="weighted",
+                multi_class="ovr"
+            ))
+            if len(class_distributions) < num_classes:
+                class_distributions = np.append(class_distributions, np.zeros(
+                    num_classes - len(class_distributions)))
+            # normalize the class distributions
+            class_distributions = class_distributions / \
+                np.sum(class_distributions)
+            tprs.append(np.array(intermediate_tprs).T @
+                        class_distributions.reshape(-1, 1))
+            precisions.append(np.array(intermediate_precisions).T @
+                              class_distributions.reshape(-1, 1))
+            auprcs.append(np.array(intermediate_auprcs).T @
+                          class_distributions.reshape(-1, 1))
+            tprs[-1] = tprs[-1].reshape(-1)
+            precisions[-1] = precisions[-1].reshape(-1)
+            auprcs[-1] = auprcs[-1][0]
+        else:
+            raise NotImplementedError
+        ax[0].plot(fpr_mean, tprs[-1],
+                   label=f"ROC Fold {fold_idx + 1} (AUC = {aurocs[fold_idx]:0.2f})", alpha=.3)
         # Precision-Recall Curve
-        precision, recall, prc_thresh = precision_recall_curve(fold["y"], fold["proba"].detach().cpu().numpy()[:, 1])
-        prec_interp = np.interp(recall_mean, recall[::-1], precision[::-1])
-        precisions.append(prec_interp)
-        recall_threshes.append(prc_thresh[np.abs(recall-0.85).argmin()])
-        auprcs.append(auc(recall, precision))
-        ax.plot(recall, precision, label=f"PRC Fold {fold_idx + 1} (AUC = {auprcs[fold_idx]:0.2f})")
-    
-    # ax[0].set_xlabel("False Positive Rate")
-    # ax[0].set_ylabel("True Positive Rate")
-    # ax[0].set_title(f"Receiver Operating Characteristic (ROC) Curve for {model_name}")
-    # ax[0].set_ylim(-0.1, 1.1)
-    
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.set_title(f"Precision-Recall Curve for {model_name}")
-    ax.set_ylim(-0.1, 1.1)
-    
+        ax[1].plot(recall_mean, precisions[-1],
+                   label=f"PRC Fold {fold_idx + 1} (AUC = {auprcs[fold_idx]:0.2f})", alpha=.3)
+
+    ax[0].set_xlabel("False Positive Rate")
+    ax[0].set_ylabel("True Positive Rate")
+    ax[0].set_title(
+        f"Receiver Operating Characteristic (ROC) Curve for {model_name}")
+    ax[0].set_ylim(-0.1, 1.1)
+
+    ax[1].set_xlabel("Recall")
+    ax[1].set_ylabel("Precision")
+    ax[1].set_title(f"Precision-Recall Curve for {model_name}")
+    ax[1].set_ylim(-0.1, 1.1)
+
     # ROC
-    # tpr_mean = np.mean(tprs, axis=0)
-    # tpr_mean[-1] = 1.0
-    # auroc_mean = auc(fpr_mean, tpr_mean)
-    # auroc_std = np.std(aurocs)
-    # ax[0].plot(
-    #     fpr_mean, tpr_mean,
-    #     label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (auroc_mean, auroc_std),
-    #     lw=2, alpha=.8
-    # )
-    # tpr_std = np.std(tprs, axis=0)
-    # ax[0].fill_between(
-    #     fpr_mean, np.maximum(tpr_mean - tpr_std, 0),
-    #     np.minimum(tpr_mean + tpr_std, 1), alpha=.2,
-    #     label=r"$\pm$ 1 std. dev.", color='grey'
-    # )
-    
+    tpr_mean = np.mean(tprs, axis=0)
+    tpr_mean[-1] = 1.0
+    auroc_mean = auc(fpr_mean, tpr_mean)
+    auroc_std = np.std(aurocs)
+    ax[0].plot(
+        fpr_mean, tpr_mean,
+        label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (auroc_mean, auroc_std),
+        lw=2, alpha=.8
+    )
+    tpr_std = np.std(tprs, axis=0)
+    ax[0].fill_between(
+        fpr_mean, np.maximum(tpr_mean - tpr_std, 0),
+        np.minimum(tpr_mean + tpr_std, 1), alpha=.2,
+        label=r"$\pm$ 1 std. dev.", color='grey'
+    )
+
     # PRC
     prec_mean = np.mean(precisions, axis=0)
     auprc_mean = auc(recall_mean, prec_mean)
     auprc_std = np.std(auprcs)
-    ax.plot(
+    ax[1].plot(
         recall_mean, prec_mean,
         label=r"Mean PRC (AUC = %0.2f $\pm$ %0.2f)" % (auprc_mean, auprc_std),
         lw=2, alpha=.8
     )
     prec_std = np.std(precisions, axis=0)
-    ax.fill_between(
+    ax[1].fill_between(
         recall_mean, np.maximum(prec_mean - prec_std, 0),
         np.minimum(prec_mean + prec_std, 1), alpha=.2,
         label=r"$\pm$ 1 std. dev.", color='grey'
     )
-    
+
     fig.suptitle(f"ROC and PRC Curves for {model_name}")
-    # ax[0].legend()
-    ax.legend()
+    ax[0].legend()
+    ax[1].legend()
     plt.show()
